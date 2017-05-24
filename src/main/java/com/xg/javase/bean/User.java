559c4f7b0fd3864b479af617a58f7e323212c884
xionggao <yanxinyu@terminus.io>
2017-05-24 14:03:15 +0800

    Initial commit

---
+++ src/main/java/com/xg/javase/bean/User.java
@@ -0,0 +1,11 @@
+package com.xg.javase.bean;
+
+/**
+ * Created by xionggao on 2017/5/24.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private String sex;
+
+}
---
+++ src/main/java/com/xg/javase/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.xg.javase.dao;
+
+/**
+ * Created by xionggao on 2017/5/24.
+ */
+public class UserDao {
+}
